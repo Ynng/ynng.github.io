57c88c47a509326973cd0a36d4d3d29309c5c648
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2019-05-16 13:49:50 -0400

    improved animation

--- index.js
+++ index.js
@@ -2,22 +2,25 @@ window.onload = function () {
     // alert("It's loaded!")
     document.getElementById("body").classList.remove('preload');
     var images = document.getElementsByClassName("image");
-    for(var i = 0, len = images.length; i<len; i++){
+    for (var i = 0, len = images.length; i < len; i++) {
         images[i].addEventListener("click", openImageOverlay)
     }
 }
 
-function openImageOverlay(){
+function openImageOverlay() {
     document.getElementById("overlay").style.transition= ".5s cubic-bezier(0.4, 1.4, 0.74, 0.96)"
-    document.getElementById("overlay").style.backgroundColor = "rgba(0, 0, 0, 0.8)"
     document.getElementById("overlay").style.top = "0"
     document.getElementById("overlay-image").src = this.src
     document.getElementById("overlay-text").innerHTML = this.alt
     document.getElementById("body").classList.add("noscroll")
+    setTimeout(function () { 
+        document.getElementById("overlay").style.backgroundColor = "rgb(45, 43, 51,0.8)" 
+        }, 350);
+
 }
 
-function closeImageOverlay(){
-    document.getElementById("overlay").style.transition= ".4s cubic-bezier(1,-0.4, 1, 0.65)"
+function closeImageOverlay() {
+    document.getElementById("overlay").style.transition = ".4s cubic-bezier(1,-0.4, 1, 0.65)"
     document.getElementById("overlay").style.backgroundColor = "rgba(0, 0, 0, 0)"
     document.getElementById("overlay").style.top = "120vh"
     document.getElementById("body").classList.remove("noscroll")
