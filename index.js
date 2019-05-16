5a31ba96d681b6a8bc400fa7631d25f473fe8d6d
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2019-05-16 11:09:43 -0400

    added overlay
    You can now click on any image and view it in a bigger size
    TODO: implrment a button to close the overlay

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 window.onload = function () {
     // alert("It's loaded!")
     document.getElementById("body").classList.remove('preload');
+    var images = document.getElementsByClassName("image");
+    for(var i = 0, len = images.length; i<len; i++){
+        images[i].addEventListener("click", openImageOverlay)
+    }
+}
+
+function openImageOverlay(){
+    document.getElementById("overlay").style.backgroundColor = "rgba(0, 0, 0, 0.8)"
+    document.getElementById("overlay").style.top = "0"
+    document.getElementById("overlay-text").innerHTML = document.getElementById("overlay-image").alt
+    document.getElementById("body").classList.add("noscroll")
 }
