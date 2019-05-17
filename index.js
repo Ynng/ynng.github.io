37010b9e8db4b693ea5b0e88d405c16b99fe83e9
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2019-05-17 11:55:52 -0400

    improved js, and buttons

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ window.onload = function () {
     // alert("It's loaded!")
     document.getElementById("body").classList.remove('preload');
     var images = document.getElementsByClassName("image");
-    for (var i = 0, len = images.length; i < len; i++) {
+    for (var i = 0; i < images.length; i++) {
         images[i].addEventListener("click", openImageOverlay)
     }
     var buttons = document.getElementsByClassName("large label");
-    for (var i = 0, len = buttons.length; i < len; i++) {
+    for (var i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener("click", openCards)
     }
 }
 
 function openImageOverlay() {
-    document.getElementById("overlay").style.transition= ".75s cubic-bezier(0.08, 0.82, 0.17, 1)"
+    document.getElementById("overlay").style.transition = ".75s cubic-bezier(0.08, 0.82, 0.17, 1)"
     document.getElementById("overlay").style.top = "0"
     document.getElementById("overlay-image").src = this.src
     document.getElementById("overlay-text").innerHTML = this.alt
@@ -25,10 +25,21 @@ function closeImageOverlay() {
     document.getElementById("body").classList.remove("noscroll")
 }
 
-function openCards(){
+function openCards() {
     this.classList.add("selected")
     var list = document.getElementsByClassName(this.id)
-    for (var i = 0, len = list.length; i < len; i++) {
+    for (var i = 0; i < list.length; i++) {
         list[i].classList.remove("hidden")
+    }
+
+    var buttons = document.getElementsByClassName("large label");
+    for (var i = 0; i < buttons.length; i++) {
+        if (buttons[i].id != this.id) {
+            buttons[i].classList.remove("selected");
+            var list = document.getElementsByClassName(buttons[i].id)
+            for (var j = 0 ; j < list.length; j++) {
+                list[j].classList.add("hidden")
+            }
+        }
     }
 }
