ac23933f930ea616239c31453259e64b052b91ec
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2019-05-21 11:58:30 -0400

    improved animation and js dramaticaly using jquery

--- index.js
+++ index.js
@@ -1,47 +1,41 @@
-window.onload = function () {
-    // alert("It's loaded!")
-    document.getElementById("body").classList.remove('preload');
-    var images = document.getElementsByClassName("image");
-    for (var i = 0; i < images.length; i++) {
-        images[i].addEventListener("click", openImageOverlay)
-    }
-    var buttons = document.getElementsByClassName("large label");
-    for (var i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener("click", openCards)
-    }
-}
+'use strict';
 
-function openImageOverlay() {
-    document.getElementById("overlay").style.transition = ".75s cubic-bezier(0.08, 0.82, 0.17, 1)"
-    document.getElementById("overlay").style.top = "0"
-    document.getElementById("overlay-image").src = this.src
-    document.getElementById("overlay-text").innerHTML = this.alt
-    document.getElementById("body").classList.add("noscroll")
-}
+$(function () {
+    $(".image").click(function () {
+        $("#overlay").css({
+            "transition": ".75s cubic-bezier(0.08, 0.82, 0.17, 1)",
+            "top": "0"
+        });
+        $("#overlay-image").attr("src", this.src);
+        $("#overlay-text").html(this.alt);
+        $("body").addClass("noscroll");
+    });
+
+    $(".large.label").click(function () {
+        $(this).addClass("selected");
+        $("." + this.id).each(function(){
+            $(this).removeClass("hidden");
+            $(this).css("height",this.scrollHeight);
+        })
+
+        $(".large.label").not("#" + this.id).each(function () {
+            $(this).removeClass("selected");
+            $("." + this.id).each(function(){
+                $(this).addClass("hidden");
+                $(this).css("height","0");
+            });
+        });
+    });
+});
+
+$(document).ready(function () {
+    $("body").removeClass("preload")
+});
 
 function closeImageOverlay() {
-    document.getElementById("overlay").style.transition = ".5s cubic-bezier(0.08, 0.82, 0.17, 1)"
-    document.getElementById("overlay").style.top = "100vh"
-    document.getElementById("body").classList.remove("noscroll")
-}
-
-function openCards() {
-    this.classList.add("selected")
-    var list = document.getElementsByClassName(this.id)
-    for (var i = 0; i < list.length; i++) {
-        list[i].style.height=list[i].scrollHeight
-        list[i].classList.remove("hidden")
-    }
-
-    var buttons = document.getElementsByClassName("large label");
-    for (var i = 0; i < buttons.length; i++) {
-        if (buttons[i].id != this.id) {
-            buttons[i].classList.remove("selected");
-            var list = document.getElementsByClassName(buttons[i].id)
-            for (var j = 0 ; j < list.length; j++) {
-                list[j].style.height=list[j].scrollHeight
-                list[j].classList.add("hidden")
-            }
-        }
-    }
+    $("#overlay").css({
+        "transition": ".5s cubic-bezier(0.08, 0.82, 0.17, 1)",
+        "top": "100vh"
+    });
+    $("body").removeClass("noscroll");
 }
