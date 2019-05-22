d090141cd26c5f59846b30d6cce57d864d1d1f52
Ynng <44907675+Ynng@users.noreply.github.com>
2019-05-22 17:27:21 -0400

    improved animation overall

--- index.js
+++ index.js
@@ -2,43 +2,37 @@
 
 $(function () {
     $(".image").click(function () {
-        $("#overlay").css({
-            "transition": ".75s cubic-bezier(0.08, 0.82, 0.17, 1)",
-            "top": "0"
-        });
+        $("#overlay").removeClass("hidden")
         $("#overlay-image").attr("src", this.src);
         $("#overlay-text").html(this.alt);
         $("body").addClass("noscroll");
     });
 
     $(".large.label").click(function () {
-        $(this).addClass("selected");
-        $('html, body').animate({
-            scrollTop: $("#main_menu").offset().top-10
-        }, 1000);
-        $("." + this.id).each(function () {
-            $(this).removeClass("hidden");
-            $(this).css("height", this.scrollHeight);
-        })
-
-        $(".large.label").not("#" + this.id).each(function () {
+        if ($(this).hasClass("selected")) return;
+        $(".large.label.selected").each(function () {
             $(this).removeClass("selected");
             $("." + this.id).each(function () {
                 $(this).addClass("hidden");
                 $(this).css("height", "0");
             });
         });
+        $(this).addClass("selected");
+        $('html, body').animate({
+            scrollTop: $("#main_menu").offset().top - 10
+        }, 500);
+        $("." + this.id).each(function () {
+            $(this).removeClass("hidden");
+            $(this).css("height", this.scrollHeight);
+        });
     });
 });
 
 $(document).ready(function () {
-    $("body").removeClass("preload")
+    $("body").removeClass("preload");
 });
 
 function closeImageOverlay() {
-    $("#overlay").css({
-        "transition": ".5s cubic-bezier(0.08, 0.82, 0.17, 1)",
-        "top": "100vh"
-    });
+    $("#overlay").addClass("hidden");
     $("body").removeClass("noscroll");
 }
