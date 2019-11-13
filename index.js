8ffe3a64c0d1c3a28aedf55b1bcef91e299c483b
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2019-11-13 09:51:36 -0500

    added blur effect?

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
 'use strict';
 
+var setBlur = function (ele, radius) {
+    $(ele).css({
+        "-webkit-filter": "blur(" + radius + "px)",
+        "filter": "blur(" + radius + "px)"
+    });
+},
+
+tweenBlur = function (ele, startRadius, endRadius) {
+    $({ blurRadius: startRadius }).animate({ blurRadius: endRadius }, {
+        duration: 500,
+        easing: 'swing', // or "linear"
+        // use jQuery UI or Easing plugin for more options
+        step: function () {
+            setBlur(ele, this.blurRadius);
+        },
+        complete: function () {
+            // Final callback to set the target blur radius
+            // jQuery might not reach the end value
+            setBlur(ele, endRadius);
+        }
+    });
+};
+
+$(document).ready(function () {
+    $("body").removeClass("preload");
+});
+
+function closeImageOverlay() {
+    $("#overlay").addClass("hidden");
+    $("body").removeClass("noscroll");
+    tweenBlur('#main-container',2,0);
+}
+
 $(function () {
+
+
     $(".image").click(function () {
         $("#overlay-image").attr("src", this.src);
         $("#overlay-text").html(this.alt);
         $("body").addClass("noscroll");
-
+        tweenBlur('#main-container',0,2);
         // Making sure the overlay image is as big as it can get while keeping the aspect ratio and stays within the screen
-        if ($("#overlay-image").width() / $(window).width() < $("#overlay-image").height() / ($(window).height()-200)) {
+        if ($("#overlay-image").width() / $("#overlay-image").height() < $(window).width() / $(window).height()) {
             $("#overlay-image").css({
                 "width": "auto",
                 "height": "75vh"
@@ -35,7 +70,7 @@ $(function () {
 
         //Select the current selection
         $(this).addClass("selected");
-        
+
         //Show the cards for the current selection
         $("." + this.id).each(function () {
             $(this).removeClass("hidden");
@@ -51,15 +86,6 @@ $(function () {
             $('html, body').animate({
                 scrollTop: $(this).offset().top - 10
             }, 500);
-        }        
+        }
     });
 });
-
-$(document).ready(function () {
-    $("body").removeClass("preload");
-});
-
-function closeImageOverlay() {
-    $("#overlay").addClass("hidden");
-    $("body").removeClass("noscroll");
-}
