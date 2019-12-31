4e0b958cf54273f5173a96b493f5add53c1cb3b3
Ynng <44907675+Ynng@users.noreply.github.com>
2019-12-31 17:13:28 -0500

    finally fixed the animations

--- index.js
+++ index.js
@@ -1,98 +1,137 @@
-'use strict';
+"use strict";
 
-var setBlur = function (ele, radius) {
-    $(ele).css({
-        "-webkit-filter": "blur(" + radius + "px)",
-        "filter": "blur(" + radius + "px)"
-    });
-},
+function setBlur(ele, radius) {
+  $(ele).css({
+    "-webkit-filter": "blur(" + radius + "px)",
+    filter: "blur(" + radius + "px)"
+  });
+}
 
-tweenBlur = function (ele, startRadius, endRadius) {
-    $({ blurRadius: startRadius }).animate({ blurRadius: endRadius }, {
-        duration: 300,
-        easing: 'swing', // or "linear"
-        // use jQuery UI or Easing plugin for more options
-        step: function () {
-            setBlur(ele, this.blurRadius);
-        },
-        complete: function () {
-            // Final callback to set the target blur radius
-            // jQuery might not reach the end value
-            setBlur(ele, endRadius);
-        }
-    });
-};
+function tweenBlur(ele, startRadius, endRadius) {
+  $({ blurRadius: startRadius }).animate(
+    { blurRadius: endRadius },
+    {
+      duration: 300,
+      easing: "swing", // or "linear"
+      // use jQuery UI or Easing plugin for more options
+      step: function() {
+        setBlur(ele, this.blurRadius);
+      },
+      complete: function() {
+        // Final callback to set the target blur radius
+        // jQuery might not reach the end value
+        setBlur(ele, endRadius);
+      }
+    }
+  );
+}
 
-$(document).ready(function () {
-    $("body").removeClass("preload");
+$(document).ready(function() {
+  $("body").removeClass("preload");
 });
 
 function closeImageOverlay() {
-    $("#overlay").addClass("hidden");
-    $("body").removeClass("noscroll");
-    tweenBlur('#main-container',2,0);
+  $("#overlay").addClass("hidden");
+  $("body").removeClass("noscroll");
+  tweenBlur("#main-container", 2, 0);
 
-    setTimeout(function () {
-        $("#overlay").addClass("disappear");
-    }, 300);
+  setTimeout(function() {
+    $("#overlay").addClass("disappear");
+  }, 300);
 }
 
-$(function () {
+var openedLabel = "about_me";
+var animatingLabels = false;
 
-    $(".image").click(function () {
-        $("#overlay-image").attr("src", this.src);
-        $("#overlay-text").html(this.alt);
-        $("body").addClass("noscroll");
-        tweenBlur('#main-container',0,2);
+function updateCardHeight() {
+  $("." + openedLabel).each(function() {
+    var style = window.getComputedStyle(this);
+    $(this).removeClass("hidden");
+    console.log(this.scrollHeight);
+    $(this).css(
+      "height",
+      this.scrollHeight -
+        Number(style.getPropertyValue("padding-top").slice(0, -2)) -
+        Number(style.getPropertyValue("padding-bottom").slice(0, -2))
+    );
+  });
+}
 
-        // Making sure the overlay image is as big as it can get while keeping the aspect ratio and stays within the screen
-        if (this.width / this.height < $(window).width() / $(window).height()) {
-            $("#overlay-image").css({
-                "width": "auto",
-                "height": "75vh"
-            });
-        } else {
-            $("#overlay-image").css({
-                "width": "95vw",
-                "height": "auto"
-            });
-        }
-        $("#overlay").removeClass("disappear");
-        
-        setTimeout(function () {
-            $("#overlay").removeClass("hidden");
-        }, 1);
-    });
+$(function() {
+  $(".image").click(function() {
+    $("#overlay-image").attr("src", this.src);
+    $("#overlay-text").html(this.alt);
+    $("body").addClass("noscroll");
+    tweenBlur("#main-container", 0, 2);
 
-    $(".button.label").click(function () {
-        //Only works on buttons that are not selected
-        if ($(this).hasClass("selected")) return;
+    // Making sure the overlay image is as big as it can get while keeping the aspect ratio and stays within the screen
+    if (this.width / this.height < $(window).width() / $(window).height()) {
+      $("#overlay-image").css({
+        width: "auto",
+        height: "75vh"
+      });
+    } else {
+      $("#overlay-image").css({
+        width: "95vw",
+        height: "auto"
+      });
+    }
+    $("#overlay").removeClass("disappear");
 
-        //Un"select" the previous selection
-        $("." + $(".button.label.selected").attr("id")).each(function () {
-            $(this).addClass("hidden");
-            $(this).css("height", "0");
+    setTimeout(function() {
+      $("#overlay").removeClass("hidden");
+    }, 1);
+  });
+
+  $(".button.label").click(function() {
+    //Only works on buttons that are not selected
+    if ($(this).hasClass("selected")) return;
+    if(animatingLabels) return;
+    animatingLabels = true;
+    //Un"select" the previous selection
+    var current = $(this);
+    updateCardHeight();
+    setTimeout(function() {
+      $("." + openedLabel).each(function() {
+        $(this).addClass("hidden");
+        $(this).css("height", "0");
+      });
+
+      //Select the current selection
+      $("#" + openedLabel).removeClass("selected");
+      $(current).addClass("selected");
+      openedLabel = $(current).attr("id");
+      console.log(openedLabel);
+
+      //Show the cards for the current selection
+      $("." + openedLabel).each(function() {
+        $(this).removeClass("hidden");
+      });
+      updateCardHeight();
+
+      setTimeout(function() {
+        $("." + openedLabel).each(function() {
+          $(this).css("height", "");
         });
-        $(".button.label.selected").removeClass("selected");
+        animatingLabels = false;
+      }, 400);
 
-        //Select the current selection
-        $(this).addClass("selected");
-
-        //Show the cards for the current selection
-        $("." + this.id).each(function () {
-            $(this).removeClass("hidden");
-            $(this).css("height", this.scrollHeight);
-        });
-
-        //Scroll to hopefully center the cards that just appeareed
-        if (window.matchMedia('(min-width: 60rem)').matches) {
-            $('html, body').animate({
-                scrollTop: $("#main_menu").offset().top - 10
-            }, 500);
-        } else {
-            $('html, body').animate({
-                scrollTop: $(this).offset().top - 10
-            }, 500);
-        }
-    });
+      //   Scroll to hopefully center the cards that just appeareed
+      if (window.matchMedia("(min-width: 60rem)").matches) {
+        $("html, body").animate(
+          {
+            scrollTop: $("#main_menu").offset().top - 10
+          },
+          400
+        );
+      } else {
+        $("html, body").animate(
+          {
+            scrollTop: $(this).offset().top - 10
+          },
+          400
+        );
+      }
+    }, 1);
+  });
 });
