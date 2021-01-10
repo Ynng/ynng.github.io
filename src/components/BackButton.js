e6edc9c3020da5b0672c76ede3de65c91077f0aa
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2021-01-09 23:51:02 -0500

    React website

---
+++ src/components/BackButton.js
@@ -0,0 +1,14 @@
+import './BackButton.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Link } from "react-router-dom";
+import { faArrowCircleLeft } from "@fortawesome/free-solid-svg-icons";
+
+function BackButton ({target}) {
+  return (
+    <Link to={target} className = "back-button">
+      <FontAwesomeIcon icon={faArrowCircleLeft}/>
+    </Link>
+  );
+
+}
+export default BackButton;
---
+++ src/components/ButtonCollection.js
@@ -0,0 +1,13 @@
+import './ButtonCollection.css';
+import LinkButton from'./LinkButton.js';
+
+function ButtonCollection({buttons, style, className}){
+  return(
+    <ul className={`button-collection ${className}`} style = {style}>
+      {buttons.map((button,idx) => (
+        <LinkButton key = {idx} button = {button}/>
+      ))}
+    </ul>
+  );
+}
+export default ButtonCollection;
